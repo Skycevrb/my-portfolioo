4a3908d849cee3f43f745852e1488d7c712eba05
Skycevrb <victoredeigbo@gmail.com>
2026-05-20 02:33:14 +0100

    website

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const counter = document.getElementById('counter');
 const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
 
+
 increaseBtn.addEventListener('click', () => {
     count++;
     counter.innerText = count;
@@ -13,3 +14,17 @@ decreaseBtn.addEventListener('click', () => {
     count--;
     counter.innerText = count;
 });
+
+//reset function
+function resetform() {
+    // confirmation before resetting
+    let confirmReset = confirm("do you want to clear the counter?");
+
+    if (confirmReset) {
+        document.getElementById("myform").reset(); alert("form has cleared");
+    }
+}
+
+function reset() {
+    document.getElementById("name").value = "";
+}
